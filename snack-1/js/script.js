a90a56dcda45ad8d7250b455a23a9a6e5c6503a7
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2023-11-10 12:17:36 +0100

    Snack completo

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,6 +1,14 @@
-const agePerson1 = (prompt("Type the age of the first person"))
-const agePerson2 = (prompt("Type the age of the second person"))
-if ( agePerson1.length)
+const agePerson1 = parseInt(prompt("Type the age of the first person"));
+const agePerson2 = parseInt(prompt("Type the age of the second person"));
+
+if (agePerson1 > agePerson2) {console.log ("The first person is the older")
+}
+
+else if (agePerson2 > agePerson1) {console.log ("The second person is the older")
+}
+
+else {console.log ("The age is the same")}
 
 
-document.getElementById("older-person").innerHTML = "The older person is " ;
+
+
