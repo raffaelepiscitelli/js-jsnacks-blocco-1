3b8774224b4e5f299fefac8cdca2cb0bc3afac3d
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2023-11-10 11:52:44 +0100

    Avanzamento snack

---
+++ snack-1/js/script.js
@@ -0,0 +1,6 @@
+const agePerson1 = (prompt("Type the age of the first person"))
+const agePerson2 = (prompt("Type the age of the second person"))
+if ( agePerson1.length)
+
+
+document.getElementById("older-person").innerHTML = "The older person is " ;
